67a97bbaba67b091e89af16e813d0b190b5cf67b
Hailee Miu <hailee.miu@gmail.com>
2019-04-27 15:12:25 -0500

    display items per category

--- grocery_list/views.py
+++ grocery_list/views.py
@@ -3,16 +3,19 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from collections import defaultdict
 from django.views.generic import CreateView, ListView
 from . import models
 
+
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+  return HttpResponse("Hello, world. You're at the polls index.")
+
 
 class ItemCreate(CreateView):
   model = models.Item
   fields = ['item', 'quantity', 'category']  
+
 
 class GroceryListView(ListView):
   model = models.Item
@@ -21,36 +24,20 @@ class GroceryListView(ListView):
 
   def get_organized_data(self):
 
-    items = models.Item.objects.all()
-    categories = models.Category.objects.all()
+    grocery_list = models.Item.objects.select_related('category')
+    items_by_category = {}
 
-    organized = {}
+    for item in grocery_list:
+      if item.category.name not in items_by_category:
+        items_by_category[item.category.name] = []
 
-    for category in categories:
-      assert isinstance(category, models.Category)
-      items = list()
-
-      organized_list = {
-        'category': category,
-        'item_list': items,
-      }
-
-      organized[category] = organized_list
-
-      specific_items = models.Item.objects.select_related('category').filter(category_id=category.id)
-
-      for item in specific_items:
-        category = specific_items.category
-        organized = organized[category]
-        item_list=organized['items']
-        items.append(item)
-
-      return organized.items()
+      items_by_category[item.category.name].append(item.item)
+    
+    return items_by_category.items()
 
   def get_context_data(self, **kwargs):
     context = super().get_context_data(**kwargs)
 
     context['grocery_list'] = self.get_organized_data()
-
-    # context['list'] = models.Item.objects.select_related('category')
     return context
+
